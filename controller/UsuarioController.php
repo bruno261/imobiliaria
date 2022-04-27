97ee8f3bad1b7f6b000ff912e6246f56361933b2
Sebastião Willian Pereira Regi <swillian2009@gmail.com>
2022-04-26 22:29:58 -0300

    Falta implementar o controller.

--- controller/UsuarioController.php
+++ controller/UsuarioController.php
@@ -1,15 +1,17 @@
 <?php
-/* 
-Logar com um usuário no sistema.
-*/
-public function logar()
-{
+    class UsuarioController
+    {
+        /* 
+        Logar com um usuário no sistema.
+        */
+        public function logar()
+        {
 
-    $usuario = new Usuario();
-    $usuario->setLogin($_POST['login']);
-    $usuario->setSenha($_POST['senha']);
+            $usuario = new Usuario();
+            $usuario->setLogin($_POST['login']);
+            $usuario->setSenha($_POST['senha']);
 
-    return $usuario->logar();
-}
-
+            return $usuario->logar();
+        }
+    }
 ?>
