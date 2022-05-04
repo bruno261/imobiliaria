e0b801c09befaf3ed767407b212796983a4a4fe4
Sebastião Willian Pereira Regi <swillian2009@gmail.com>
2022-05-03 22:16:01 -0300

    Continue...

--- controller/UsuarioController.php
+++ controller/UsuarioController.php
@@ -1,22 +1,22 @@
 <?php
+    require_once '../model/Usuario.php';
 
-require_once '../model/Usuario.php';
+    class UsuarioController
+    {
+        public static function salvar()
+        {
+            $usuario = new Usuario();
+            $usuario->setLogin($_POST['login']);
+            $usuario->setSenha($_POST['senha1']);
+            $usuario->setPermissao($_POST['permissao']);
 
-class UsuarioController{
-    public static function salvar(){
-        $usuario = new Usuario();
-        $usuario->setLogin($_POST['login']);
-        $usuario->setSenha($_POST['senha1']);
-        $usuario->setPermissao($_POST['permissao']);
+            $usuario->save();
+        }
 
-        $usuario->save();
+        public static function listar()
+        {
+            $usuario = new Usuario();
+            return $usuario->listAll();
+        }
     }
-
-    public static function listar(){
-        $usuario = new Usuario();
-        return $usuario->listAll();
-    }
-
-}
-
 ?>
